fd3143cc3375d8d4621fb0b4802860cc70b9e1ba
David Hurrell <dhurrell@hotmail.co.uk>
2026-05-12 18:50:01 +0100

    add route to register to print posted data to console

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 #  route for the home page, renders the home.html template
@@ -14,7 +14,20 @@ def login():
 
 # route for the register page, renders the register.html template
 @app.route("/register", methods=["GET", "POST"])
+#  Print register form data to console for testing purposes, to show handling of posted data
+
 def register():
+    # Check if the request method is POST meaning form has been submitted
+    if request.method == "POST":
+        # get the form data
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        # print the form data to the console
+        print("Username:", username)
+        print("Email:", email)
+        print("Password:", password)
+
     return render_template("register.html")
 # route for the new post page, renders the newPost.html template
 @app.route("/new_post", methods=["GET", "POST"])
